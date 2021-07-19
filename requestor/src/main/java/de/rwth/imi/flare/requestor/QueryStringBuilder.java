c1c2c8c75b27d5aaca0c60dba24ccbbfb73e7069
lszimtenings <lukas.szimtenings@rwth-aachen.de>
2021-07-19 09:10:52 +0200

    Url encode params

--- requestor/src/main/java/de/rwth/imi/flare/requestor/QueryStringBuilder.java
+++ requestor/src/main/java/de/rwth/imi/flare/requestor/QueryStringBuilder.java
@@ -4,11 +4,15 @@ import de.rwth.imi.flare.api.model.Criterion;
 import de.rwth.imi.flare.api.model.TerminologyCode;
 import de.rwth.imi.flare.api.model.FilterType;
 import de.rwth.imi.flare.api.model.ValueFilter;
+import de.rwth.imi.flare.api.model.mapping.FixedCriteria;
 import de.rwth.imi.flare.api.model.mapping.MappingEntry;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class QueryStringBuilder {
     private final Criterion criterion;
-    private final StringBuilder builder;
+    private final StringBuilder sb;
 
     public static String constructQueryString(Criterion searchCriterion){
         QueryStringBuilder builder = new QueryStringBuilder(searchCriterion);
@@ -16,45 +20,53 @@ public class QueryStringBuilder {
     }
 
     private QueryStringBuilder(Criterion searchCriterion){
-        this.builder = new StringBuilder();
+        this.sb = new StringBuilder();
         this.criterion = searchCriterion;
     }
 
     private String constructQueryString(){
         MappingEntry mapping = this.criterion.getValueFilter().getMapping();
-        this.builder.append(mapping.getFhirResourceType()).append('?');
+        this.sb.append(mapping.getFhirResourceType()).append('?');
+
         if(this.criterion.getValueFilter() != null){
             appendValueFilterByType();
         }
-        // Search for concepts
         else{
             appendConceptFilterString();
         }
 
         if(mapping.getFixedCriteria() != null){
-
+            appendFixedCriteriaString();
         }
 
-        return builder.toString();
+        return sb.toString();
+    }
+
+    private void appendFixedCriteriaString() {
+        for (FixedCriteria criterion : this.criterion.getValueFilter().getMapping().getFixedCriteria()){
+            this.sb.append('&').append(criterion.getSearchParameter()).append('=').append(criterion.getCode());
+        }
     }
 
     private void appendConceptFilterString() {
         MappingEntry mapping = this.criterion.getValueFilter().getMapping();
+        StringBuilder sbTemp = new StringBuilder();
         // TODO: Tree expansion if necessary
         TerminologyCode termCode = this.criterion.getTermCode();
-        this.builder.append(mapping.getTermCodeSearchParameter())
-                .append('=')
-                .append(termCode.getCode()).append('|').append(termCode.getSystem());
+        this.sb.append(mapping.getTermCodeSearchParameter()).append('=');
+        sbTemp.append(termCode.getSystem()).append('|').append(termCode.getCode());
+        this.sb.append(urlEncodeAndReset(sbTemp));
     }
 
     private void appendValueFilterByType() {
         MappingEntry mapping = this.criterion.getValueFilter().getMapping();
+        StringBuilder sbTemp = new StringBuilder();
 
         if(mapping.getTermCodeSearchParameter() != null){
             TerminologyCode termCode = this.criterion.getTermCode();
-            builder.append(mapping.getTermCodeSearchParameter())
-                    .append('=')
-                    .append(termCode.getSystem()).append('|').append(termCode.getCode());
+            sb.append(mapping.getTermCodeSearchParameter()).append('=');
+            sbTemp.append(termCode.getSystem()).append('|').append(termCode.getCode());
+            sb.append(urlEncodeAndReset(sbTemp));
         }
         FilterType filter = this.criterion.getValueFilter().getFilter();
         if (filter == FilterType.QUANTITY_COMPARATOR){
@@ -63,22 +75,39 @@ public class QueryStringBuilder {
         else if (filter == FilterType.QUANTITY_RANGE){
             appendQuantityRangeFilterString();
         }
-        //TODO: Implement concept filter
+        //TODO: handle FilterType.CONCEPT
     }
 
     private void appendQuantityRangeFilterString() {
         ValueFilter valueFilter = this.criterion.getValueFilter();
         MappingEntry mapping = valueFilter.getMapping();
         String valueSearchParameter = mapping.getValueSearchParameter();
-        builder.append(valueSearchParameter).append("=ge").append(valueFilter.getMinValue()).append(valueFilter.getUnit());
-        builder.append('&');
-        builder.append(valueSearchParameter).append("=le").append(valueFilter.getMaxValue()).append(valueFilter.getUnit());
+        StringBuilder sbTemp = new StringBuilder();
+
+        sb.append(valueSearchParameter).append("=");
+        sbTemp.append("ge ").append(valueFilter.getMinValue()).append(valueFilter.getUnit());
+        sb.append(urlEncodeAndReset(sbTemp)).append('&');
+
+        sb.append(valueSearchParameter).append("=");
+        sbTemp.append("le ").append(valueFilter.getMaxValue()).append(valueFilter.getUnit());
+        sb.append(urlEncodeAndReset(sbTemp));
     }
 
     private void appendQuantityComparatorFilterString() {
         ValueFilter valueFilter = this.criterion.getValueFilter();
         String valueSearchParameter = this.criterion.getValueFilter().getMapping().getValueSearchParameter();
-        this.builder.append(valueSearchParameter).append('=')
-                .append(valueFilter.getComparator()).append(valueFilter.getValue()).append(valueFilter.getUnit());
+        StringBuilder sbTemp = new StringBuilder();
+
+        this.sb.append(valueSearchParameter).append('=');
+        sbTemp.append(valueFilter.getComparator()).append(valueFilter.getValue()).append(valueFilter.getUnit());
+        this.sb.append(urlEncodeAndReset(sbTemp));
+    }
+
+    private static String urlEncodeAndReset(StringBuilder strBuilder){
+        String encoded = URLEncoder.encode(strBuilder.toString(), StandardCharsets.UTF_8);
+        // Reset StringBuilder
+        strBuilder.setLength(0);
+        strBuilder.trimToSize();
+        return encoded;
     }
 }
