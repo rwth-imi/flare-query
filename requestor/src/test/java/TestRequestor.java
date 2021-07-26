2abe50ca897924efa3c546ef644882151792e71e
lszimtenings <lukas.szimtenings@rwth-aachen.de>
2021-07-26 13:59:19 +0200

    preparations for a test

---
+++ requestor/src/test/java/TestRequestor.java
@@ -0,0 +1,28 @@
+import de.rwth.imi.flare.api.FlareResource;
+import de.rwth.imi.flare.requestor.Request;
+import org.junit.jupiter.api.Test;
+
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class TestRequestor {
+    @Test
+    public void testRequest() throws URISyntaxException {
+        String t = "https://localhost:9443/fhir-server/api/v4/Observation?code=I_COVAS_COV_M030_LAB_PARA_Q040&value-quantity=lt40";
+        Request request = new Request(new URI(t), new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(
+                        "fhiruser",
+                        "change-password".toCharArray());
+            }
+        });
+        for (Request it = request; it.hasNext(); ) {
+            FlareResource res = it.next();
+            System.out.println(res.getPatientId());
+
+        }
+    }
+}
