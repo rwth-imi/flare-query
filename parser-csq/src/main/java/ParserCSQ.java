42c4b1631a30a6eb1d09f4d3d672dc7c80f6f9c3
lszimtenings <lukas.szimtenings@rwth-aachen.de>
2021-08-12 09:24:12 +0200

    Created Codex structured query parser module

---
+++ parser-csq/src/main/java/ParserCSQ.java
@@ -0,0 +1,9 @@
+import de.rwth.imi.flare.api.FlareParser;
+import de.rwth.imi.flare.api.model.Query;
+
+public class ParserCSQ implements FlareParser {
+    @Override
+    public Query parse(String input) {
+        return null;
+    }
+}
