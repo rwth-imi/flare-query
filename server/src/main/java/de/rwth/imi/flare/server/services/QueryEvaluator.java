38eabe802192bf4774547146c1295ae72da46cd2
lszimtenings <lukas.szimtenings@rwth-aachen.de>
2021-10-14 11:01:46 +0200

    Provide Executor and Mapper as bean

--- server/src/main/java/de/rwth/imi/flare/server/services/QueryEvaluator.java
+++ server/src/main/java/de/rwth/imi/flare/server/services/QueryEvaluator.java
@@ -1,10 +1,22 @@
 package de.rwth.imi.flare.server.services;
 
+import de.rwth.imi.flare.api.Executor;
+import de.rwth.imi.flare.api.FhirResourceMapper;
 import de.rwth.imi.flare.server.QueryFormat;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QueryEvaluator {
+
+    private final Executor executor;
+    private final FhirResourceMapper mapper;
+
+    public QueryEvaluator(Executor executor, FhirResourceMapper mapper){
+
+        this.executor = executor;
+        this.mapper = mapper;
+    }
+
     public int evaluate(String query, QueryFormat format){
         return 0;
     }
