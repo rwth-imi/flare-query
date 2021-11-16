99841629aba9de06282eaf61b637b7b057d5a561
lszimtenings <lukas.szimtenings@rwth-aachen.de>
2021-11-16 15:46:10 +0100

    [FIX] Enums in XML not being parsed correctly

--- flare-api/src/main/java/de/rwth/imi/flare/api/model/xml/CaseInsensitiveComparatorAdapter.java
+++ flare-api/src/main/java/de/rwth/imi/flare/api/model/xml/CaseInsensitiveComparatorAdapter.java
@@ -3,11 +3,20 @@ package de.rwth.imi.flare.api.model.xml;
 import de.rwth.imi.flare.api.model.Comparator;
 import jakarta.xml.bind.annotation.adapters.XmlAdapter;
 
+import java.util.Locale;
+
 public class CaseInsensitiveComparatorAdapter extends XmlAdapter<String, Comparator>
 {
     @Override
     public Comparator unmarshal(String v) {
-        return Comparator.valueOf(v.toUpperCase().trim());
+        Comparator comparator;
+        try{
+            comparator = Comparator.valueOf(v.toUpperCase().trim());
+        }
+        catch (IllegalArgumentException ignore){
+            comparator = Comparator.valueOf(v.toLowerCase().trim());
+        }
+        return comparator;
     }
 
     @Override
