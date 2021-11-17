203070295875fcd7b164f5c2ef88043cf0120db5
lszimtenings <lukas.szimtenings@rwth-aachen.de>
2021-11-17 13:01:53 +0100

    [FIX] Test failed because constructor wasn't updated

--- parser-i2b2/src/test/java/de/rwth/imi/flare/parser/i2b2/TestI2b2Parser.java
+++ parser-i2b2/src/test/java/de/rwth/imi/flare/parser/i2b2/TestI2b2Parser.java
@@ -28,22 +28,22 @@ class TestI2b2Parser {
 
         TerminologyCode male_terminology = new TerminologyCode("\\\\i2b2_DEMO\\i2b2\\Demographics\\Gender\\Male\\", "i2b2_sim", "Male");
         Criterion criterion1 = new Criterion(male_terminology,
-                null, null);//new ValueFilter(FilterType.CONCEPT, new TerminologyCode[]{male_terminology}, null, null, null, null, null, null));
+                null, null, null, null);//new ValueFilter(FilterType.CONCEPT, new TerminologyCode[]{male_terminology}, null, null, null, null, null, null));
         CriteriaGroup criteriaGroup1 = new CriteriaGroup(List.of(criterion1));
 
         criterion1 = new Criterion(
                 new TerminologyCode("\\\\i2b2_DIAG\\i2b2\\Measurements\\Lymphozyten\\", "i2b2_sim", "Lymphozyten"),
-                new ValueFilter(FilterType.QUANTITY_COMPARATOR, null, Comparator.lt, 40.0, createUnit("%"), null, null), null);
+                new ValueFilter(FilterType.QUANTITY_COMPARATOR, null, Comparator.lt, 40.0, createUnit("%"), null, null), null, null, null);
         Criterion criterion2 = new Criterion(
                 new TerminologyCode("\\\\i2b2_DIAG\\i2b2\\Measurements\\Lymphozyten_absolut\\", "i2b2_sim", "Lymphozyten - absolut"),
-                new ValueFilter(FilterType.QUANTITY_COMPARATOR, null, Comparator.lt, 3.0, createUnit("/nl"), null, null), null);
+                new ValueFilter(FilterType.QUANTITY_COMPARATOR, null, Comparator.lt, 3.0, createUnit("/nl"), null, null), null, null, null);
         CriteriaGroup criteriaGroup2 = new CriteriaGroup(List.of(criterion1, criterion2));
 
 
         criterion1 = new Criterion(new TerminologyCode("\\\\i2b2_DIAG\\i2b2\\Measurements\\Bilirubin\\", "i2b2_sim", "Bilirubin (gesamt)"),
-                new ValueFilter(FilterType.QUANTITY_COMPARATOR, null, Comparator.lt, 8.0, createUnit("mg/dl"), null, null), null);
+                new ValueFilter(FilterType.QUANTITY_COMPARATOR, null, Comparator.lt, 8.0, createUnit("mg/dl"), null, null), null, null, null);
         criterion2 = new Criterion(new TerminologyCode("\\\\i2b2_DIAG\\i2b2\\Measurements\\Bilirubin_direkt\\", "i2b2_sim", "Bilirubin (direkt)"),
-                new ValueFilter(FilterType.QUANTITY_COMPARATOR, null, Comparator.lt, 6.0, createUnit("mg/dl"), null, null), null);
+                new ValueFilter(FilterType.QUANTITY_COMPARATOR, null, Comparator.lt, 6.0, createUnit("mg/dl"), null, null), null, null, null);
         CriteriaGroup criteriaGroup3 = new CriteriaGroup(List.of(criterion1, criterion2));
 
         Query expectedResult = new Query();
