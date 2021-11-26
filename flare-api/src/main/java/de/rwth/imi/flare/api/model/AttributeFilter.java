79db387b1fe9e1226e64b80103009839c1f95503
lszimtenings <lukas.szimtenings@rwth-aachen.de>
2021-11-26 13:16:55 +0100

    [FIX] renamed selectedConcepts to mirror Structured Query

--- flare-api/src/main/java/de/rwth/imi/flare/api/model/AttributeFilter.java
+++ flare-api/src/main/java/de/rwth/imi/flare/api/model/AttributeFilter.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class AttributeFilter {
     private FilterType type;
     private TerminologyCode attributeCode;
-    private List<TerminologyCode> selectedConcept;
+    private List<TerminologyCode> selectedConcepts;
     @XmlJavaTypeAdapter(CaseInsensitiveComparatorAdapter.class)
     private Comparator comparator;
     private Double value;
