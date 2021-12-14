5aa2568ddac7327fa8fab1556b9980d8fafe33fa
Julian Gründner <juliangruendner@googlemail.com>
2021-12-14 11:32:24 +0100

    fix: change port for init testdate back to 8080 for ci

--- init-testdata.sh
+++ init-testdata.sh
@@ -4,5 +4,5 @@ FILES=./testdata/*
 for fhirBundle in $FILES
 do
   echo "Sending Testdate bundle $fhirBundle ..."
-  curl -X POST -H "Content-Type: application/json" -d @$fhirBundle http://localhost:8082/fhir
+  curl -X POST -H "Content-Type: application/json" -d @$fhirBundle http://localhost:8080/fhir
 done
