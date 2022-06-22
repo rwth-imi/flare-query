45a0f788d4fb0a1b261aa858cab955c231978d2c
mhellmonds <mhellmonds@ukaachen.de>
2022-06-22 16:41:39 +0200

    implemented hashmap as cache

--- executor/src/main/java/de/rwth/imi/flare/executor/FhirIdRequestor.java
+++ executor/src/main/java/de/rwth/imi/flare/executor/FhirIdRequestor.java
@@ -5,6 +5,8 @@ import de.rwth.imi.flare.api.model.Criterion;
 import de.rwth.imi.flare.requestor.FhirRequestor;
 import de.rwth.imi.flare.requestor.FhirRequestorConfig;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
@@ -13,20 +15,39 @@ import java.util.stream.Collectors;
 public class FhirIdRequestor {
     FhirRequestorConfig config;
     Executor futureExecutor;
+    Map<Criterion, Set<String>> cache;
 
     public FhirIdRequestor(FhirRequestorConfig config, Executor futureExecutor){
         this.config = config;
         this.futureExecutor = futureExecutor;
+        this.cache = new HashMap<>();
     }
 
     /**
      * Get all ids fulfilling a given criterion
      */
     public CompletableFuture<Set<String>> getPatientIdsFittingCriterion(Criterion criterion) {
-        // TODO bessere wart/testbarkeit, Interface zum mocking auslagern oder Lambda Function-Interface
-        FhirRequestor requestor = new FhirRequestor(config);
-        return CompletableFuture.supplyAsync(() -> requestor.execute(criterion)
-                .map(FlareResource::getPatientId)
-                .collect(Collectors.toSet()), futureExecutor);
+        if(isCached(criterion)){
+            return getCachedPatientIdsFittingCriterion(criterion);
+        }else{
+            FhirRequestor requestor = new FhirRequestor(config);
+            CompletableFuture<Set<String>> ret = CompletableFuture.supplyAsync(() -> requestor.execute(criterion)
+                    .map(FlareResource::getPatientId)
+                    .collect(Collectors.toSet()), futureExecutor);
+            return ret.thenApply(idSet -> addCachedPatientIdsFittingCriterion(criterion, idSet));
+        }
+    }
+
+    private Set<String> addCachedPatientIdsFittingCriterion(Criterion criterion, Set<String> idSet) {
+        cache.put(criterion, idSet);
+        return idSet;
+    }
+
+    private boolean isCached(Criterion criterion) {
+        return cache.containsKey(criterion);
+    }
+
+    private CompletableFuture<Set<String>> getCachedPatientIdsFittingCriterion(Criterion criterion) {
+        return CompletableFuture.completedFuture(cache.get(criterion));
     }
 }
